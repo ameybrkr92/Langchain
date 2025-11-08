dbe06d140ba9dbce63af7c6197e50317199a2066
Amey Borker <ameyborker_22@yahoo.co.in>
2025-11-08 23:34:19 +0530

    LLM-OpenAI

---
+++ LLM-Models/Openai.py
@@ -0,0 +1,10 @@
+import os
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = init_chat_model("gpt-4.1")
+response = model.invoke("What is Langchain & Langgraph")
+print(response.content)
+
