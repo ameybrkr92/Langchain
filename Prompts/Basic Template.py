d199f2730be0facbaf41a2fbfe16b056d7bfa9c2
Amey Borker <ameyborker_22@yahoo.co.in>
2025-11-15 14:54:32 +0530

    learnig about .format on prompttemplate

--- Prompts/Basic Template.py
+++ Prompts/Basic Template.py
@@ -56,14 +56,42 @@ response = model.invoke(formatted)
 print(response.content)
 """
 
-prompt_multi_input = PromptTemplate(input_variables=[], template="Tell me a fact about {topic} for a {level} student")
+prompt_multi_input = PromptTemplate(input_variables=['topic','level'], template="Tell me a fact about {topic} for a {level} student")
 print(prompt_multi_input)
+#input_variables=['level', 'topic'] input_types={} partial_variables={} template='Tell me a fact about {topic} for a {level} student'
 print(type(prompt_multi_input))
-print(prompt_multi_input.format_prompt({'topic':"Jupiter", 'level':"Graduate"}))
-print(type(prompt_multi_input.format_prompt({'topic':"Jupiter", 'level':"Graduate"})))
-#formatted_prompt=prompt_multi_input.format(topic="Jupyter", level ="graduate")
+#<class 'langchain_core.prompts.prompt.PromptTemplate'>
+
+#using .format_prompt or .format
+'''
+👉 StringPromptTemplate
+A base class that defines:
+Formatting rules
+Template types (f-string, jinja2, mustache)
+String-specific behavior
+
+👉 PromptTemplate
+A higher-level wrapper that:
+Adds input validation (input_variables)
+Adds .format() and .format_prompt() helpers
+Simplifies usage with LLMs
+'''
+#strictly speaking, the implementation of format_prompt lives in the base class (StringPromptTemplate). 
+# But since PromptTemplate inherits from StringPromptTemplate, you use it as if it were part of PromptTemplate. It’s not exclusive to PromptTemplate, but you will call it on PromptTemplate objects in practice.
+
+formatted_prompt_1=prompt_multi_input.format(topic="Jupiter", level="Graduate")
+#or formatted_prompt=prompt_multi_input.format(topic="Jupyter", level ="graduate")
+print(prompt_multi_input.format(topic="Jupiter", level="Graduate"))
+#text='Tell me a fact about Jupiter for a Graduate student'
+print(type(prompt_multi_input.format(topic="Jupiter", level="Graduate")))
+#<class 'langchain_core.prompt_values.StringPromptValue'>
+
+
 formatted_prompt=prompt_multi_input.invoke({'topic':"Jupiter", 'level':"Graduate"})
 print(formatted_prompt)
+#text='Tell me a fact about Jupiter for a Graduate student'
 print(type(formatted_prompt))
+#<class 'langchain_core.prompt_values.StringPromptValue'>
+
 response=model.invoke(formatted_prompt)
 print(response.content)
