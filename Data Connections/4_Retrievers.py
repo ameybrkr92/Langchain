5ca10bd0aca986eccae052d191b160d0d6443955
Amey Borker <ameyborker_22@yahoo.co.in>
2025-11-20 07:50:38 +0530

    retrievers

---
+++ Data Connections/4_Retrievers.py
@@ -0,0 +1,37 @@
+#Document Loader
+from langchain_community.document_loaders import PyPDFLoader
+
+pdf_loader = PyPDFLoader("Data Connections\Attention is all you need - Research paper.pdf")
+
+pdf_docs = pdf_loader.load()
+    
+#Text Splitting
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size = 200,
+    chunk_overlap = 50
+)
+
+chunks = splitter.split_documents(pdf_docs)
+
+#embeddings + vector store
+from langchain_openai import OpenAIEmbeddings
+from langchain_chroma import Chroma
+
+embedding_model = OpenAIEmbeddings()
+
+vectorstore = Chroma.from_documents(
+    documents=chunks,
+    embedding=embedding_model,
+    persist_directory="Data Connections/chroma_db"
+) 
+
+retriever = vectorstore.as_retriever(
+    search_type="similarity",
+    search_kwargs={"k": 4}
+)
+
+retrieved_docs=retriever.invoke("What is the pdf about")
+
+print(retrieved_docs)
